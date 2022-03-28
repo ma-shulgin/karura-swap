fe084a7ea7695e52d7de742b8e19860218753b89
ma-shulgin <95925978+ma-shulgin@users.noreply.github.com>
2022-03-28 15:12:54 +0300

    Initial commit

---
+++ src/types/events.ts
@@ -0,0 +1,64 @@
+import assert from 'assert'
+import {EventContext, Result, deprecateLatest} from './support'
+import * as v9130 from './v9130'
+
+export class BalancesTransferEvent {
+  constructor(private ctx: EventContext) {
+    assert(this.ctx.event.name === 'balances.Transfer')
+  }
+
+  /**
+   *  Transfer succeeded (from, to, value, fees).
+   */
+  get isV1020(): boolean {
+    return this.ctx._chain.getEventHash('balances.Transfer') === '72e6f0d399a72f77551d560f52df25d757e0643d0192b3bc837cbd91b6f36b27'
+  }
+
+  /**
+   *  Transfer succeeded (from, to, value, fees).
+   */
+  get asV1020(): [Uint8Array, Uint8Array, bigint, bigint] {
+    assert(this.isV1020)
+    return this.ctx._chain.decodeEvent(this.ctx.event)
+  }
+
+  /**
+   *  Transfer succeeded (from, to, value).
+   */
+  get isV1050(): boolean {
+    return this.ctx._chain.getEventHash('balances.Transfer') === 'dad2bcdca357505fa3c7832085d0db53ce6f902bd9f5b52823ee8791d351872c'
+  }
+
+  /**
+   *  Transfer succeeded (from, to, value).
+   */
+  get asV1050(): [Uint8Array, Uint8Array, bigint] {
+    assert(this.isV1050)
+    return this.ctx._chain.decodeEvent(this.ctx.event)
+  }
+
+  /**
+   * Transfer succeeded.
+   */
+  get isV9130(): boolean {
+    return this.ctx._chain.getEventHash('balances.Transfer') === '0ffdf35c495114c2d42a8bf6c241483fd5334ca0198662e14480ad040f1e3a66'
+  }
+
+  /**
+   * Transfer succeeded.
+   */
+  get asV9130(): {from: v9130.AccountId32, to: v9130.AccountId32, amount: bigint} {
+    assert(this.isV9130)
+    return this.ctx._chain.decodeEvent(this.ctx.event)
+  }
+
+  get isLatest(): boolean {
+    deprecateLatest()
+    return this.isV9130
+  }
+
+  get asLatest(): {from: v9130.AccountId32, to: v9130.AccountId32, amount: bigint} {
+    deprecateLatest()
+    return this.asV9130
+  }
+}
